913ec6b2ce8f560f03021727da107b11cc12ee15
wcleung <leungwingchung60@gmail.com>
2026-06-14 17:03:13 +0800

    [twgec][misc] Update test bash for parallel smoke test

--- test/run-test.sh
+++ test/run-test.sh
@@ -3,12 +3,18 @@ TEST_DIR="test"
 SMOKE_DIR="example"
 
 unit_test() {
-    local path=$1
-    local test_file="$1/test.twge"
-    local test_result="$1/output.events"
-    local test_error="$1/error.log"
-    local test_print="$1/print.twge"
-    option=""
+    local test_file="$1"
+    local path=$(dirname $test_file)
+    local test_result="$path/output.events"
+    local test_error="$path/error.log"
+    local test_print="$path/print.twge"
+    local output_dir="$path/output"
+    mkdir -p "$output_dir"
+    local output_print="$output_dir/print.twge"
+    local output_event="$output_dir/output.events"
+    local output_error="$output_dir/error.log"
+    local option=""
+
     if [[ -s "$test_file" ]]; then
         # Fail case
         if [[ -s $test_error ]] && [[ ! -s $test_result ]] && [[ ! -s $test_print ]]; then
@@ -16,73 +22,77 @@ unit_test() {
             if [[ $first_line == //\ OPTION:\ * ]]; then
                 option="${first_line:11}"
             fi
-            eval "$BUILD_DIR/twgec $test_file $option" 2> error.log
-            if diff error.log $test_error ; then
+            $BUILD_DIR/twgec $test_file $option -o=$output_event 2> $output_error
+            if diff $output_error $test_error ; then
                 echo "twge test success:    $test_file"
             else
                 echo -e "\033[0;31mtwge test fail:       $test_file\033[0m"
+                exit 1
             fi
-            rm error.log
         # Pass case
         elif [[ -s $test_result ]] && [[ ! -s $test_error ]] && [[ ! -s $test_print ]]; then
-            eval $BUILD_DIR/twgec $test_file 2> error.log
-            if [[ ! -s error.log ]]; then
-                if diff game.events "$test_result"; then
+            $BUILD_DIR/twgec $test_file -o=$output_event 2> $output_error
+            if [[ ! -s $output_error ]]; then
+                if diff $output_event "$test_result"; then
                     echo "twge test success:    $test_file"
                 else
                     echo -e "\033[0;31mtwge test fail:       $test_file\033[0m"
+                    exit 1
                 fi
             else
-                cat error.log
+                cat $output_error
                 echo -e "\033[0;31mtwge test fail:       $test_file\033[0m"
-                rm error.log
+                exit 1
             fi
-            rm game.events
         # Pass case with print
         elif [[ -s $test_print ]] && [[ ! -s $test_result ]] && [[ ! -s $test_error ]]; then
-            eval $BUILD_DIR/twgec $test_file 2> error.log
+            $BUILD_DIR/twgec $test_file 2> $output_error
             first_line=$(head -n 1 $test_file)
             if [[ $first_line == //\ OPTION:\ * ]]; then
                 command="${first_line:11}"
-                eval "$BUILD_DIR/twgec $test_file $command" 2> error.log 1> print.twge 
-                if diff print.twge "$test_print"; then
+                $BUILD_DIR/twgec $test_file $command -o=$output_event 2> $output_error 1> $output_print
+                if diff $output_print "$test_print"; then
                     echo "twge test success:    $test_file"
                 else
                     echo -e "\033[0;31mtwge test fail:       $test_file\033[0m"
+                    exit 1
                 fi
             else
                 echo "The first line does not start with '// OPTION: '"
                 exit 1
             fi
-            rm error.log
-            rm print.twge
-            rm game.events
         else
             echo "Please provide exactly ONE \"output.events\", \"error.log\", or \"print.twge\" under \"$path\""
         fi
     fi
+    rm -rf $output_dir
 }
 
 smoke_test() {
     local input_file="$1"
-    local ast_after_file="$(dirname "$input_file")/output/ast-after.twge"
-    local output_file="$(dirname "$input_file")/output/output.events"
-    $BUILD_DIR/twgec "$input_file" -o=$output_file 2> error.log
-    if [[ ! -s error.log ]]; then
-        $BUILD_DIR/twgec "$input_file" --print-ast-after > $ast_after_file 
+    local path=$(dirname "$input_file")
+    local output_ast="$path/output/ast-after.twge"
+    local output_event="$path/output/output.events"
+    local output_error="$path/output/error.log"
+
+    $BUILD_DIR/twgec "$input_file" -o=$output_event 2> $output_error
+    if [[ ! -s $output_error ]]; then
+        $BUILD_DIR/twgec "$input_file" --print-ast-after > $output_ast 
         echo "twge test success:    $input_file"
     else
-        cat error.log
+        cat $output_error
         echo -e "\033[0;31mtwge test fail:       $input_file\033[0m"
+        exit 1
     fi
-    rm game.events
-    rm error.log
+    rm $output_error
 }
 
-for dir in $(find $TEST_DIR -type d); do
-    unit_test $dir
+for file in $(find $SMOKE_DIR -type f -name "main.twge"); do
+    smoke_test $file &
 done
 
-for file in $(find $SMOKE_DIR -type f -name "main.twge"); do
-    smoke_test $file
+for file in $(find $TEST_DIR -type f -name "test.twge"); do
+    unit_test $file
 done
+
+wait
